4c2e7c9eb3c98d0001a1210f499e2cb3eb58ca3f
jhsharp <73131537+jhsharp@users.noreply.github.com>
2022-03-05 21:20:01 -0600

    Goal Zones

---
+++ PuzzleGameUnity/Assets/Scripts/GoalZone.cs
@@ -0,0 +1,74 @@
+/****
+ * Created by: Jacob Sharp
+ * Date Created: March 5, 2022
+ * 
+ * Last Edited by: Jacob Sharp
+ * Date Last Edited: March 5, 2022
+ * 
+ * Description: Controls the goal zone to move to the next level
+ ****/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalZone : MonoBehaviour
+{
+    public string nextScene; // the scene to move to upon level completion
+
+    private int xMin, xMax, yMin, yMax;
+    private bool transitioning;
+    private float transitionTimer;
+    public float transitionDelay;
+
+    private Collider2D col;
+    private BlockManager manager;
+    private PlayerCore core;
+
+    void Start()
+    {
+        col = GetComponent<Collider2D>(); // get a reference to the collider
+        manager = FindObjectOfType<BlockManager>(); // get a reference to the block manager
+        core = FindObjectOfType<PlayerCore>(); // get a reference to the player core
+
+        // Set the bounds of the goal area based on the collider dimensions
+        xMin = (int)Mathf.Floor((transform.position.x - (col.bounds.extents.x - 0.05f) - manager.transform.position.x) / manager.blockSize);
+        xMax = (int)Mathf.Floor((transform.position.x + (col.bounds.extents.x - 0.05f) - manager.transform.position.x) / manager.blockSize);
+        yMin = (int)Mathf.Floor((transform.position.y - (col.bounds.extents.y - 0.05f) - manager.transform.position.y) / manager.blockSize);
+        yMax = (int)Mathf.Floor((transform.position.y + (col.bounds.extents.y - 0.05f) - manager.transform.position.y) / manager.blockSize);
+    }
+
+    void Update()
+    {
+        if (!transitioning) // if the level is not already complete, check for completion
+        {
+            if (checkComplete()) // if the level was just finished, start transitioning to the next level
+            {
+                transitioning = true;
+                transitionTimer = transitionDelay;
+                core.freezeBlocks(true);
+            }
+        }
+        else transition(); // continue transitioning to the next stage
+    }
+
+    public bool checkComplete() // indicates whether all player controlled blocks are within the goal area
+    {
+        foreach (PlayerBlock blk in core.playerBlocks)
+        {
+            if (blk.x < xMin || blk.x > xMax || blk.y < yMin || blk.y > yMax) return false; // return false if there is a block outside the goal area
+        }
+        return true; // otherwise return true
+    }
+
+    private void transition() // manages the transition process to the next stage
+    {
+        if (transitionTimer <= 0)
+        {
+            // switch scenes
+            Debug.Log("DONE!!!");
+        }
+        else transitionTimer -= Time.deltaTime; // otherwise continue counting down to the scene switch
+    }
+}
